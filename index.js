8e7d3256f91881e6aac7455380b73cc67c6e3790
beccabay <rbay@seattleu.edu>
2020-04-08 13:49:19 -0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 let myVue = new Vue({
     el: "#app",
     data: {
-    message: "Hello World!"
+        message: "Hello World!"
     }
-  });
+});
 
-  
+let listVue = new Vue({
+    el: "#list-app",
+    data: {
+        list: ["flour", "eggs", "salt", "baking powder", "potatoes", "onions"]
+    }
+});
+
+let ifVue = new Vue({
+    el: "#if-app",
+    data: {
+        seeable: true
+    },
+    methods: {
+        toggleMessage: function () {
+            this.seeable = !this.seeable;
+        }
+    }
+});
